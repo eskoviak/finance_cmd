25788c217e63a4671f9ac83698051e7ff3e2711f
Ed Skoviak <eskoviak@gmail.com>
2022-08-31 13:17:36 -0500

    updated sql

--- queries/voucher_entry.sql
+++ queries/voucher_entry.sql
@@ -1,4 +1,4 @@
 -- Active: 1660319329194@@127.0.0.1@5432@finance@public
-select finance.insert_voucher('8/17/22 12:30 CDT','',35,2,2003,4,19998,'Red Wing, MN');
+select finance.insert_voucher('8/28/22 13:50 CDT','',52.69,1,3016,6,28538,'Bloomington, MN');
 
-select finance.insert_voucher_detail(1088,1,'050101',35,'Haircut','','29 plus 6 tip');
+select finance.insert_voucher_detail(1116,1,'050101',52.69,'Electronics','','');
