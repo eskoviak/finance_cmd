cc19d9173e8cf02c60356a1ab379caa0d2e875f5
Ed Skoviak <eskoviak@gmail.com>
2022-09-09 08:17:14 -0500

    periodic update

--- queries/voucher_entry.sql
+++ queries/voucher_entry.sql
@@ -1,4 +1,4 @@
 -- Active: 1660319329194@@127.0.0.1@5432@finance@public
-select finance.insert_voucher('8/28/22 13:50 CDT','',52.69,1,3016,6,28538,'Bloomington, MN');
+select finance.insert_voucher('9/8/22 16:31 CDT','',43.89,2,2000,6,26624,'Red Wing, MN');
 
-select finance.insert_voucher_detail(1116,1,'050101',52.69,'Electronics','','');
+select finance.insert_voucher_detail(1135,2,'050101',6.48,'Grocery','','');
