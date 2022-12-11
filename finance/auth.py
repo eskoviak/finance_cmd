169022644dda5f232fd109a48d723081ce6b8264
Ed Skoviak <eskoviak@gmail.com>
2022-12-11 17:51:25 -0600

    finishing up this sprint

--- finance/auth.py
+++ finance/auth.py
@@ -6,7 +6,7 @@ from pg_utils import PgUtils
 from models_tst import User
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -17,7 +17,7 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        pg_utils = PgUtils()
+        pg_utils = PgUtils(current_app.config['PGURI'])
 
         error = None
         if not username:
@@ -41,7 +41,7 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        pg_utils = PgUtils()
+        pg_utils = PgUtils(current_app.config['PGURI'])
         user = pg_utils.get_user_by_name(username)
         error = None
         if len(user.keys()) == 0:
@@ -61,7 +61,7 @@ def login():
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-    pg_utils = PgUtils()
+    pg_utils = PgUtils(current_app.config['PGURI'])
     if user_id == None:
         g.user = None
     else:
