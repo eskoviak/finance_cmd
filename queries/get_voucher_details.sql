cb768e81c162cfa50ca46828cbd04a91afa6c1b9
Ed Skoviak <eskoviak@gmail.com>
2022-12-24 19:18:42 -0500

    updated nb and sql

--- queries/get_voucher_details.sql
+++ queries/get_voucher_details.sql
@@ -1,9 +1,13 @@
-SELECT voucher_date
+SELECT v.voucher_date
+  ,v.voucher_number
   ,(SELECT vendor_short_desc FROM finance.vendors WHERE vendor_number = v.vendor_number)
-  ,account_number
-  ,dimension_1
-  ,dimension_2
-FROM voucher v
+  ,vd.account_number
+  ,vd.split_seq_number
+  ,vd.dimension_1
+  ,vd.dimension_2
+FROM finance.voucher v
 JOIN finance.voucher_detail vd on v.voucher_number = vd.voucher_number
-WHERE account_number = '050103'
-ORDER by vendor_short_desc, voucher_date
+WHERE account_number IN (
+  SELECT DISTINCT account_number FROM finance.voucher_detail
+)
+ORDER by voucher_date, split_seq_number
