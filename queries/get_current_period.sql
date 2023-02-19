713c1f16b33e60ffd175ee056971a2075cf42d7e
Ed Skoviak <eskoviak@gmail.com>
2023-02-19 12:48:12 -0600

    updated query

--- queries/get_current_period.sql
+++ queries/get_current_period.sql
@@ -1,4 +1,5 @@
 -- Active: 1660319329194@@127.0.0.1@5432@finance
+set search_path = 'finance';
 select id
   ,invoice_id
   ,v.vendor_short_desc
@@ -10,6 +11,11 @@ select id
 from accounts_payable ap
 join vendors v on ap.vendor_number = v.vendor_number
 join external_accounts e on ap.payment_source_id = e.external_account_id
-where payment_due_dt between '2/3/23' and '2/16/23'
---  and payment_voucher_id is NULL
+where payment_due_dt between '2/17/23' and '3/3/23'
+  and payment_voucher_id is NULL
 order by payment_due_dt;
+
+select sum(stmt_amt) AS Total
+from accounts_payable
+where payment_due_dt between '2/17/23' and '3/3/23'
+  and payment_voucher_id is NULL;
