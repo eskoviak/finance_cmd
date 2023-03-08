069533a39af30549e0085d1184a552c1038d7a06
Ed Skoviak <eskoviak@gmail.com>
2023-03-07 18:25:25 -0600

    update

--- queries/update_acc_payable.sql
+++ queries/update_acc_payable.sql
@@ -1,4 +1,15 @@
+
+--declare @voucher_id = INTEGER
+--declare @stmt_amt = FLOAT
+--declare @id = INTEGER
+--set @voucher_id = 1517
+--set @stmt_amt = 1852.88
+--set @id = 27
+
+--with (vn, sa, ai) as (values (1517, 1852.88, 27))
+
+
 update accounts_payable
-set payment_voucher_id = 1516,
-stmt_amt = 142.56
-where id = 7;
+set payment_voucher_id = 1517,
+stmt_amt = 1852.88
+where id = 27;
