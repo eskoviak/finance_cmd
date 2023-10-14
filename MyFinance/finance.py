3badd5327a4209bd4d6073ed940c56e576f7642e
Ed Skoviak <eskoviak@gmail.com>
2023-10-13 19:38:40 -0500

    changed finance.py  program to use .env

--- MyFinance/finance.py
+++ MyFinance/finance.py
@@ -4,6 +4,7 @@ sys.path.append('/Users/edmundlskoviak/Documents/repos/finance_cmd')
 from flask import Flask, render_template, request, redirect, url_for
 #from archive.models_tst import Voucher, VoucherDetail
 from MyFinance.utils.pg_utils import PgUtils
+import os
 
 
 # The application factory
@@ -28,7 +29,8 @@ def create_app(test_config=None):
     if test_config is None:
         # load the instance config, if it exists, when not testing
         # was 'config.py'
-        app.config.from_pyfile('config.py', silent=True)
+        # app.config.from_pyfile('config.py', silent=True)
+        app.config['PGURI'] = os.environ.get('PGURI')
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
